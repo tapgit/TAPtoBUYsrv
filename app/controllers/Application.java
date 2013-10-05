88b593289a976034ba8b43dd191f89751a7b909c
kebinbin <kevin.castillo@upr.edu>
2013-10-05 02:32:04 -0400

    Login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import org.codehaus.jackson.JsonNode;
+
 import play.*;
 import play.mvc.*;
 
@@ -10,5 +12,21 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-  
+    
+    public static Result checkLogin(){
+    	JsonNode json = request().body().asJson();
+    	  if(json == null) {
+    	    return badRequest("Expecting Json data");
+    	  } 
+    	  else {
+    	    String username = json.findPath("username").getTextValue();
+    	    String password = json.findPath("password").getTextValue();
+    	    if(username.equals("Lolo") && password.equals("bond")){
+    	      return ok("You're now signed in");
+    	    }
+    	    else{
+    	    	return unauthorized("Bad username or password");
+    	    }
+    	  }
+    }
 }
